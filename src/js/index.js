fe168011a3db5720f43c04d9b2fd8cc368504f1b
fr4nbtt <batet.francisco@gmail.com>
2025-05-14 18:06:59 -0300

    chore: keep tailwind import in js, finalize html shell

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,11 @@
 import '../styles/output.css';
 import { createIcons, icons } from 'lucide';
 
-import { greet } from './modules/utils.js';
-console.log(greet('Starter!'));
-
-const greetBabel = (name) => {
-  console.log(`Hello, ${name}!`);
-};
-
-greetBabel('World');
-
 const iconSpan = document.getElementById('icon');
 if (iconSpan) {
   iconSpan.innerHTML =
-    '<i data-lucide="landmark" width="40" height="40" class="inline"></i>';
+    '<i data-lucide="cloud-lightning" width="48" height="48" class="inline"></i>';
   createIcons({ icons });
 } else {
   console.warn('No element with id="icon" found.');
 }
-
-createIcons({ icons });
