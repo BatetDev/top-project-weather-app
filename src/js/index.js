570c200d45ecfa0b16d8acb03967f4f3c72c0b3f
fr4nbtt <batet.francisco@gmail.com>
2025-05-20 15:51:16 -0300

    feat: import and use processWeatherData in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,18 @@
 import '../styles/output.css';
 import { createIcons, icons } from 'lucide';
 import { fetchWeather } from './modules/api.js';
+import { processWeatherData } from './modules/processWeatherData.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const iconSpan = document.getElementById('icon');
+  const form = document.getElementById('weather-form');
+  const input = document.getElementById('location-input');
+
   if (iconSpan) {
     iconSpan.innerHTML =
       '<i data-lucide="cloud-lightning" width="48" height="48" class="inline"></i>';
     createIcons({ icons });
   }
-
-  const form = document.getElementById('weather-form');
-  const input = document.getElementById('location-input');
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -21,6 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!location) return;
 
     console.log('Fetching weather for:', location);
-    const weatherData = await fetchWeather(location);
+
+    const rawData = await fetchWeather(location);
+    const weather = processWeatherData(rawData);
+
+    console.log('Processed Weather Data:', weather);
   });
 });
