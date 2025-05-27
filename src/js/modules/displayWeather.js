df5154a770011d378be716165ad0b31be79bf8ad
fr4nbtt <batet.francisco@gmail.com>
2025-05-27 14:30:48 -0300

    style: update spacing, colors & other minor tweaks

--- src/js/modules/displayWeather.js
+++ src/js/modules/displayWeather.js
@@ -11,8 +11,8 @@ export function displayWeather(weather) {
   let precipHTML = '';
   if (weather.precipProb > 0 && weather.precipAmount > 0) {
     precipHTML = `
-      <p class="border-b border-zinc-800/30"><span class="text-zinc-800">Precipitation:</span> ${weather.precipAmount} mm</p>
-      <p class="border-b border-zinc-800/30"><span class="text-zinc-800">Chance of Rain:</span> ${weather.precipProb}%</p>
+      <p class="border-b border-zinc-800/40"><span class="text-zinc-800/90">Precipitation:</span> ${weather.precipAmount} mm</p>
+      <p class="border-b border-zinc-800/40"><span class="text-zinc-800/90">Chance of Rain:</span> ${weather.precipProb}%</p>
     `;
   }
 
@@ -20,23 +20,23 @@ export function displayWeather(weather) {
   const doomDescription = doomMap[weather.icon] || doomMap['unknown'];
 
   output.innerHTML = `
-    <div class="bg-vintage-pattern p-4 max-w-md w-full mx-auto border-4 border-double border-zinc-900 shadow-[5px_5px_0_0_rgba(24,24,27,1)]">
-      <h2 class="text-3xl text-zinc-900 mb-4 text-center">${weather.location}</h2>
+    <div class="bg-vintage-pattern p-4 max-w-md w-full mx-auto border-4 border-double border-zinc-900/90">
+      <h2 class="text-3xl text-zinc-900/90 mb-4 text-center">${weather.location}</h2>
       
       <div class="flex justify-center mb-4">
-        <i data-lucide="${icon}" class="w-16 h-16 text-purple-950/90"></i>
+        <i data-lucide="${icon}" class="w-16 h-16 text-purple-900/90"></i>
       </div>
 
       <p class="text-3xl mb-2 text-center">${weather.temperature}°C</p>
-      <p class="text-zinc-800 mb-4 text-center font-body">Feels like ${weather.feelsLike}°C</p>
+      <p class="text-zinc-800/90 mb-4 text-center font-body">Feels like ${weather.feelsLike}°C</p>
 
       <div class="space-y-2 text-left text-sm mb-4">
-        <p class="italic text-zinc-50 text-lg text-center font-body bg-zinc-900 px-4 py-3 mb-4 border border-zinc-900 rounded-sm">"${doomDescription}"</p>
+        <p class="italic text-zinc-50 text-lg text-center font-body bg-zinc-900/95 px-2.5 py-3 mb-4 border border-zinc-900/90 rounded-xs">${doomDescription}</p>
         
-        <div class="font-body space-y-1 border-t-2 border-b-2 border-zinc-900 py-3 px-2">
-          <p class="border-b border-zinc-800/30"><span class="text-zinc-800">Conditions:</span> ${weather.conditions}</p>
-          <p class="border-b border-zinc-800/30"><span class="text-zinc-800">Humidity:</span> ${weather.humidity}%</p>
-          <p class="border-b border-zinc-800/30"><span class="text-zinc-800">Wind Speed:</span> ${weather.windSpeed} km/h</p>
+        <div class="font-body space-y-1 border-t-2 border-b-2 border-zinc-900/90 py-3 px-1.5">
+          <p class="border-b border-zinc-800/40"><span class="text-zinc-800/90">Conditions:</span> ${weather.conditions}</p>
+          <p class="border-b border-zinc-800/40"><span class="text-zinc-800/90">Humidity:</span> ${weather.humidity}%</p>
+          <p class="border-b border-zinc-800/40"><span class="text-zinc-800/90">Wind Speed:</span> ${weather.windSpeed} km/h</p>
           ${precipHTML}
         </div>
       </div>
