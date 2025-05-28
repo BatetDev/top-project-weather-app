62628aefb786ad3df1f303999dd60e673a911391
fr4nbtt <batet.francisco@gmail.com>
2025-05-28 19:04:27 -0300

    feat: add minimum dealy to loading component in loading.js

--- src/js/modules/loading.js
+++ src/js/modules/loading.js
@@ -1,17 +1,28 @@
 // modules/loading.js
 
+let isLoading = false;
+let timeoutId;
+
+const MIN_LOADING_TIME = 500; // ms
+
 export function showLoading() {
   const loading = document.getElementById('loading');
 
-  if (loading) {
+  if (loading && !isLoading) {
+    isLoading = true;
     loading.classList.remove('hidden');
   }
 }
 
 export function hideLoading() {
-  const loading = document.getElementById('loading');
+  if (!isLoading) return;
 
-  if (loading) {
-    loading.classList.add('hidden');
-  }
+  // Spinner stays visible for at least 500ms
+  setTimeout(() => {
+    const loading = document.getElementById('loading');
+    if (loading) {
+      loading.classList.add('hidden');
+    }
+    isLoading = false;
+  }, MIN_LOADING_TIME);
 }
